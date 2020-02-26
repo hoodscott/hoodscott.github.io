fbdd33573a9d398eac12eb31dee0ef73ab977ecf
Scott Hood <scottdothood@gmail.com>
2020-02-26 23:06:27 +0000

    Creating templates for projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,26 @@
+const markdownIt = require('markdown-it');
+const markdownItAttrs = require('markdown-it-attrs');
+
 module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('css');
+
+  /* Add markdown library for adding html attributes */
+  const mdOptions = {
+    html: true
+  };
+  const markdownLib = markdownIt(mdOptions).use(markdownItAttrs);
+  eleventyConfig.setLibrary('md', markdownLib);
 
   return {
     dir: {
       input: 'src',
       includes: '_includes',
       data: '_data',
-      output: '_site'
+      output: '_site' 
     },
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
-    templateFormats: ['html', 'njk', 'md']
-
+    templateFormats: ['html', 'njk', 'md'],
   }
 }
---
+++ src/project/cairds.md
@@ -0,0 +1,12 @@
+---
+title: cairds
+description: card 'game' builder and player
+date: 2018-05-01
+layout: project.njk
+---
+
+### Header 1
+Description Goes Here
+
+### Header 2
+More text can go here
