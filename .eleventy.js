1b736899def02481273eade04e47766e597c004d
Scott Hood <scottdothood@gmail.com>
2020-02-26 21:37:24 +0000

    Breaking page into template
    Should make it easier to make new pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,15 @@
 module.exports = eleventyConfig => {
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy('css');
 
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: '_site'
+    },
+    markdownTemplateEngine: 'njk',
+    templateFormats: ['html', 'njk', 'md']
 
   }
 }
