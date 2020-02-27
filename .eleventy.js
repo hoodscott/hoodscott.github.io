e0014272533c733f24295f53d38d759138296fb5
Scott Hood <scottdothood@gmail.com>
2020-02-27 21:31:09 +0000

    Can use containers in markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const markdownIt = require('markdown-it');
 const markdownItAttrs = require('markdown-it-attrs');
+const markdownItCont = require('markdown-it-container');
 const htmlMin = require('html-minifier');
 
 module.exports = eleventyConfig => {
@@ -9,7 +10,16 @@ module.exports = eleventyConfig => {
   const mdOptions = {
     html: true
   };
-  const markdownLib = markdownIt(mdOptions).use(markdownItAttrs);
+  const markdownLib = markdownIt(mdOptions);
+  markdownLib.use(markdownItAttrs);
+  markdownLib.use(markdownItCont, 'section', {
+    validate: function(params) {
+      return params.trim().match(/^section$/);
+    },
+    render: function (tokens, idx) {
+      return (tokens[idx].nesting === 1) ? '<section class="colour-section">' : '</section>';
+    }
+  });
   eleventyConfig.setLibrary('md', markdownLib);
 
   /* Minifying the html files with a transform */
