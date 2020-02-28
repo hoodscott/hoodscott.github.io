ce0884a5eb462b796f98bf165757c1772f4f3ea6
Scott Hood <scottdothood@gmail.com>
2020-02-28 00:35:07 +0000

    Adding some fun date filters

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,27 @@ const markdownItAttrs = require('markdown-it-attrs');
 const markdownItCont = require('markdown-it-container');
 const htmlMin = require('html-minifier');
 
+/* Maps breakpoints in Roman numberals */
+const romanMapping = [
+  [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+  [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+  [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'],
+  [1, 'I'],
+]
+
+/* Converts a number to its Roman numeral string representation */
+function romanNumeral(num) {
+  /* Recursive base case */
+  if (num == 0) return '';
+  /* Find the highest number smaller than the input number */
+  for (pair of romanMapping) {
+    if (pair[0] <= num) {
+      /* Add the required letter(s) and run again with remainder */
+      return pair[1] + romanNumeral(num - pair[0]);
+    }
+  }
+}
+
 module.exports = eleventyConfig => {
   /* Pass the asset folders through to the build destination folder */
   eleventyConfig.addPassthroughCopy('css');
@@ -35,6 +56,26 @@ module.exports = eleventyConfig => {
       return minified;
     }
     return content;
+  });
+
+  /* Add a filter to convert a date to its year represented
+     as a roman numeral */
+  eleventyConfig.addFilter("romanYear", function(value) {
+    const dt = new Date(value);
+    if (dt) {
+      return romanNumeral(dt.getFullYear());
+    }
+    return null;
+  });
+  /* Add a filter to convert a date object to an ISO date string */
+  eleventyConfig.addFilter("isoDate", function(value) {
+    const dt = new Date(value);
+    if (dt) {
+      return dt.getFullYear() + '-' +
+              dt.getMonth() + '-' +
+              dt.getDate();
+    }
+    return null;
   });
 
   /* Configuring the folders for the data and templates,
